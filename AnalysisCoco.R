4f1c6da06919e92ab84189122b23c265968ad547
Coco Kusiak <cokusiak@gmail.com>
2017-11-28 21:46:45 -0500

    cv.lasso creating some issues

--- AnalysisCoco.R
+++ AnalysisCoco.R
@@ -1,13 +1,15 @@
 require(glmnet)
 require(MASS)
 require(pROC)
+require(mosaic)
 load("mental_final.Rda")
 
 
 ment <- subset(mhfp, select = - c(trans, sexorient))
-ment <- filter(ment, complete.cases(ment))
+ment <- ment[complete.cases(ment), ]
+x <- subset(x = ment, select = -mental)
 
-x<-as.matrix(subset(ment, select = - mental))
+x<-as.matrix(x)
 y<-as.matrix(ment$mental)
 
 #LASSO
@@ -39,8 +41,10 @@ stepAIC(binom, direction= "both")
 model.AIC <- glm(mental ~ state + sleep + sex + employ + actlimit + 
                    metro + exer30 + race + income + smoker + binge, family = "binomial", 
                  data = ment)
-preds.AIC <- predict(model.AIC, newx = x, type = "response", family = "binomial")
+preds.AIC <- predict(model.AIC, newx = x, type = "response")
 
+
+#Model Comparisons
 ment <- mutate(ment, y.lasso = preds.lasso, y.ridge = preds.ridge)
 rmse <- function(truths, predictions){
   error <- truths - predictions
@@ -53,9 +57,13 @@ rmse.ridge
 
 roc.lasso <- roc(predictor = ment$y.lasso, response = ment$mental)
 roc.ridge <- roc(predictor = ment$y.ridge, response = ment$mental)
+roc.aic <- roc(predictor = preds.AIC, response = ment$mental)
 auc.lasso <- auc(roc.lasso)
 auc.ridge <- auc(roc.ridge)
-
+auc.aic <- auc(roc.aic)
+paste0("Lasso has an AUC of ", round(auc.lasso,4))
+paste0("Ridge has an AUC of ", round(auc.ridge, 4))
+paste0("AIC has an AUC of ", round(auc.aic, 4))
 
 #LASSO seems like the better of the two options
 testing <- subset(ment, select = -c(y.ridge, mental, y.lasso))
@@ -64,11 +72,15 @@ testing <- mutate(testing, y = as.numeric(predict(cv.lasso, newx = x, type = "cl
 test.x<-as.matrix(subset(testing, select = - y))
 test.y<-as.matrix(testing$y)
 
-cv.lasso <- cv.glmnet(test.x, test.y, family = "binomial", type.measure = "auc", alpha = 1)
-plot(cv.lasso)
-coef(cv.lasso)
 
+#Can LASSO find the true model?
+cv.lasso.test <- cv.glmnet(test.x, test.y, family = "binomial", type.measure = "auc", alpha = 1)
+plot(cv.lasso.test)
+coef(cv.lasso.test)
 
-cv.lasso <- cv.glmnet(test.x, test.y, family = "binomial", type.measure = "auc", alpha = 1)
-plot(cv.lasso)
-coef(cv.lasso)
+#Can Ridge find the true model?
+cv.ridge.test <- cv.glmnet(test.x, test.y, family = "binomial", type.measure = "auc", alpha = 0)
+plot(cv.ridge.test)
+coef(cv.ridge.test)
+
+#Can stepAIC find the true model?
